5ca77e6b171622db7ef76ec696ca6a50bea4ae2b
cedricSMLA <soumced97625@gmail.com>
2024-05-21 21:29:01 +0200

    Réduire la marge entre les deux premières sections sur la page Quisuisje

--- src/pages/Quisuisje.js
+++ src/pages/Quisuisje.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 //Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
@@ -8,9 +9,24 @@ import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
 import ScrollTop from "../components/ScrollTop";
 
+const Container = styled(motion.div)`
+  & > *:nth-child(1) {
+    margin-bottom: -80px; /* Ajustez cette valeur selon vos besoins */
+  }
+  & > *:nth-child(2) {
+    margin-bottom: -20px; /* Ajustez cette valeur selon vos besoins */
+  }
+  & > *:nth-child(3) {
+    margin-bottom: 20px; /* Ajustez cette valeur selon vos besoins */
+  }
+  & > *:nth-child(4) {
+    margin-bottom: 0;
+  }
+`;
+
 const Quisuisje = () => {
   return (
-    <motion.div
+    <Container
       exit="exit"
       variants={pageAnimation}
       initial="hidden"
@@ -20,7 +36,7 @@ const Quisuisje = () => {
       <ServicesSection />
       <FaqSection />
       <ScrollTop />
-    </motion.div>
+    </Container>
   );
 };
 
